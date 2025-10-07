a0fec9ea1014d88689212f94b82723b69972a85f
User <User@DESKTOP-O8LGB13>
2025-10-07 15:57:34 +0300

    changed async behavior

--- async-demo/Program.cs
+++ async-demo/Program.cs
@@ -1,26 +1,30 @@
 ﻿using System.Diagnostics;
 
 var syncStopwatch = Stopwatch.StartNew();
-ProcessData("Файл 1");
-ProcessData("Файл 2");
-ProcessData("Файл 3");
+Console.WriteLine(ProcessData("Файл 1"));
+Console.WriteLine(ProcessData("Файл 2"));
+Console.WriteLine(ProcessData("Файл 3"));
 syncStopwatch.Stop();
 
 var asyncStopWatch = Stopwatch.StartNew();
-await Task.WhenAll(ProcessDataAsync("АФайл 1"), ProcessDataAsync("АФайл 2"), ProcessDataAsync("АФайл 3"));
-asyncStopWatch.Stop();
 
+var task1 = ProcessDataAsync("АФайл 1").ContinueWith(t => Console.WriteLine(t.Result));
+var task2 = ProcessDataAsync("АФайл 2").ContinueWith(t => Console.WriteLine(t.Result));
+var task3 = ProcessDataAsync("АФайл 3").ContinueWith(t => Console.WriteLine(t.Result));
+
+await Task.WhenAll(task1, task2, task3);
+asyncStopWatch.Stop();
 Console.WriteLine($"Синхронное выполнение заняло: {syncStopwatch.Elapsed.TotalSeconds}с.");
 Console.WriteLine($"Асинхронное выполнение заняло: {asyncStopWatch.Elapsed.TotalSeconds}с.");
 
-void ProcessData(string dataName)
+string ProcessData(string dataName)
 {
     Thread.Sleep(3000);
-    Console.WriteLine($"Обработка {dataName} завершина за 3 секунды");
+    return $"Обработка {dataName} завершина за 3 секунды";
 }
 
-async Task ProcessDataAsync(string dataName)
+async Task<string> ProcessDataAsync(string dataName)
 {
     await Task.Delay(3000);
-    Console.WriteLine($"Обработка {dataName} завершина за 3 секунды");
+    return $"Обработка {dataName} завершина за 3 секунды";
 }
